79120e419ab2b15d2fc53f2bc44891a5a1daab8e
Dauntless <psywulf@gmail.com>
2023-09-04 16:48:49 +0800

    Added Ansible modules and ctop

--- installscript.sh
+++ installscript.sh
@@ -11,3 +11,8 @@ apt-get update
 apt-get install -y azure-cli powershell
 pwsh -command Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 apt-get install -y python3 python3-pip nano git jq lsb-release python3-apt python3-distutils ansible nano git iputils-ping software-properties-common apt-utils
+ansible-galaxy collection install ansible.windows
+ansible-galaxy collection install azure.azcollection
+curl -s https://api.github.com/repos/bcicen/ctop/releases/latest | grep browser_download_url|grep linux-amd64 | cut -d '"' -f 4 | wget -i -
+chmod +x ctop-*-linux-amd64
+mv ctop-*-linux-amd64 /usr/local/bin/ctop
